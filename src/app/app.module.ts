c35cb0109d79a70c47376cc5ddc9b8262bf98b0d
Alchemist Bounty <alchemist.bounty@protonmail.com>
2021-11-17 20:49:21 -0500

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+
+import { WeatherDetails } from './weatherDetails/weatherDetails.component';
+//Angular Router Module
+import { RouterModule, Router } from '@angular/router';
+import {RouterTestingModule} from '@angular/router/testing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherDetails
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterTestingModule,
+    RouterModule.forRoot([
+      { path:'', component: WeatherDetails }
+    ])
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas : [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
