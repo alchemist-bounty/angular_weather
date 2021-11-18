9943ffd588b833aa84b04b6ace6910006eaf9c9d
Alchemist Bounty <alchemist.bounty@protonmail.com>
2021-11-18 07:09:27 -0500

    update code

--- src/app/weatherFinder/weatherFinder.component.ts
+++ src/app/weatherFinder/weatherFinder.component.ts
@@ -1,4 +1,5 @@
 
+import { HttpClient } from '@angular/common/http';
 import {Component, OnInit} from '@angular/core';
 
 interface CityWeather {
@@ -23,13 +24,43 @@ interface ApiResponse {
 export class WeatherFinder implements OnInit {
 
   city: string = '';
-  cityData: any = {};
+  cityData: any = null;
+  url: string = "https://jsonmock.hackerrank.com/api/weather?name=";
+  isSunny: boolean = false;
+
+  constructor(private http:HttpClient) {
+
+  }
 
   ngOnInit() {
     // console.log(this.weatherData);
   }
 
+
   search() {
+    console.log("search");
+    console.log(this.city);
+    this.http.get(this.url + this.city).subscribe(
+      (res: any) => {
+        if (res.data.length > 0) {
+          this.cityData = res.data[0];
+          console.log(this.cityData.weather);
+          console.log(parseFloat(this.cityData.weather));
+          if (parseFloat(this.cityData.weather)>=20) {
+            this.isSunny =  true;
+          }
+          else {
+            this.isSunny =  false;
+          }
+        }
+        else {
+          this.cityData = null;
+        }
+        
+        console.log(this.cityData);
+      }
+    )
+    // HttpClient.
     // this.cityData = <data>this.weatherData.find(function(obj) {
     //   return (obj.name.toLowerCase() === value.toLowerCase());
     // });
