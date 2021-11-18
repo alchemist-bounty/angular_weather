996a2074a2528a9f08d3ee1379616b3508ab1d52
Alchemist Bounty <alchemist.bounty@protonmail.com>
2021-11-17 22:44:49 -0500

    add the html code

--- src/app/weatherDetails/weatherDetails.component.ts
+++ src/app/weatherDetails/weatherDetails.component.ts
@@ -9,16 +9,18 @@ import {Component, Input, OnInit} from '@angular/core';
 export class WeatherDetails implements OnInit {
   @Input() weatherData: data[] =[];
   city: string = '';
+  cityData: data = <data>{};
 
   ngOnInit() {
     console.log(this.weatherData);
   }
 
   cityChange(value: string) {
-    let cityData = this.weatherData.find(function(obj) {
+    this.cityData = <data>this.weatherData.find(function(obj) {
       return (obj.name.toLowerCase() === value.toLowerCase());
     });
-    console.log(cityData);
+    console.log(this.cityData);
+    
     this.city = value;
   }
 }
